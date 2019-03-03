af2fcd6cc66948873f780d30fcc19a53f95a41ca
tf-labo <7550e2f2e4d42edc551a6@gmail.com>
2019-03-03 09:35:44 +0000

    移行作業

--- tech_blog/blog/admin.py
+++ tech_blog/blog/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.conf.urls import url
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
 # Register your models here.
+
+from .models import Top, Article, ArticleTag
+
+
+@admin.register(Top)
+class TopAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'update_at', 'is_used')
+    ordering = ['update_at']
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('created_date',
+                    'updated_date',
+                    'title',
+                    'article',
+                    'get_employee',
+                    'get_member',
+                    'get_tags',
+                    'is_topic')
+
+    def get_employee(self, obj):
+        return ",".join([e.name for e in obj.employee.all()])
+
+    def get_member(self, obj):
+        return ",".join([m.name for m in obj.member.all()])
+
+    def get_tags(self, obj):
+        return ",".join([m.name for m in obj.tags.all()])
+
+
+@admin.register(ArticleTag)
+class ArticleTagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
